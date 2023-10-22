1670b8f9bc9fcd1ab2898cc24dfa09302dfc0842
isucon <isucon@isucon.aa>
2023-10-22 08:56:04 +0000

    watch journalctl

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -5,3 +5,5 @@ sudo cp ../common/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 cd ../app/go && go build -o isucari
 
 sudo systemctl restart isucari.golang.service
+
+sudo journalctl -u isucari.golang.service
