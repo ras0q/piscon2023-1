4e5bdaffe7a2d76b839cdb4ddbe8c191ac9a95b7
isucon <isucon@isucon.aa>
2023-10-22 11:25:09 +0000

    slowlog

--- s1/deploy.sh
+++ s1/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+jk
 set -aux
 
 sudo cp ../common/etc/nginx/nginx.conf /etc/nginx/nginx.conf
@@ -17,10 +17,11 @@ sudo chmod +r /var/log/mysql/mysql-slow.log /var/log/nginx/access.log /var/log/n
 sudo touch /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
 sudo chmod +r /var/log/mysql/mysql-slow.log /var/log/nginx/access.log
 sudo chown mysql:mysql /var/log/mysql/mysql-slow.log
-sudo mysql -uroot -e 'SET GLOBAL long_query_time = 0; SET GLOBAL slow_query_log = ON; SET GLOBAL slow_query_log_file = "/var/log/mysql/mysql-slow.log";'
 
 sudo systemctl restart mysql.service
 sudo systemctl restart nginx.service
 sudo systemctl restart isucari.golang.service
+
+sudo mysql -uroot -e 'SET GLOBAL long_query_time = 0; SET GLOBAL slow_query_log = ON; SET GLOBAL slow_query_log_file = "/var/log/mysql/mysql-slow.log";'
 
 sudo journalctl -u isucari.golang.service -f
