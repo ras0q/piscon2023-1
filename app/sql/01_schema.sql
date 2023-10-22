a256d2b177e459df3704087262840a46d6e8c332
ras0q <ras0q@users.noreply.github.com>
2023-10-22 23:35:04 +0900

    :sparkles: add index to items

--- app/sql/01_schema.sql
+++ app/sql/01_schema.sql
@@ -32,6 +32,7 @@ CREATE TABLE `items` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
+ALTER TABLE `items` ADD INDEX `idx_seller_id`(`seller_id`);
 
 DROP TABLE IF EXISTS `transaction_evidences`;
 CREATE TABLE `transaction_evidences` (
